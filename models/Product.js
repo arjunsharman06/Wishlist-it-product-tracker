eebabeb0484ee576a20211e744e299653c278b0b
RubinoD1 <Rubinod1@gmail.com>
2022-08-10 22:35:40 -0400

    added models for category and product -- started index.js

---
+++ models/Product.js
@@ -0,0 +1,59 @@
+// import important parts of sequelize library
+const { Model, DataTypes } = require('sequelize');
+// import our database connection from config.js
+const sequelize = require('../config/connection');
+
+// Initialize Product model (table) by extending off Sequelize's Model class
+class Product extends Model {}
+
+// set up fields and rules for Product model
+Product.init(
+  {
+      // define an id column
+      id: {
+          type: DataTypes.INTEGER,
+          allowNull: false,
+          primaryKey: true,
+          autoIncrement: true
+      },
+      // define product_name column
+      product_name: {
+          type: DataTypes.STRING,
+          allowNull: false
+      },
+      // define price column
+      price: {
+          type: DataTypes.DECIMAL(10,2),
+          allowNull: true,
+          //validation check for decimal 
+          validate: {
+              isDecimal: true
+          }
+      },
+      // define stock column
+      stock: {
+          type: DataTypes.INTEGER,
+          allowNull: false,
+          //validation check for numeric value 
+          validate: {
+              isNumeric: true
+          }
+      },
+      // define category_id column
+      category_id: {
+          type: DataTypes.INTEGER,
+          references: {
+              model: "category",
+              key: "id"
+          }
+      }
+  },
+  {
+    sequelize,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'product',
+  }
+);
+
+module.exports = Product;
---
+++ models/index.js
@@ -0,0 +1,22 @@
+// import models
+const Product = require('./Product');
+const Category = require('./Category');
+
+
+// Products belongsTo Category
+
+Product.belongsTo(Category, {
+  foreignKey: 'category_id'
+});
+
+// Categories have many Products
+
+Category.hasMany(Product, {
+  foreignKey: 'category_id'
+});
+
+
+module.exports = {
+  Product,
+  Category
+};
