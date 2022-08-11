d66fa55b5fbff36823b9820ace7b8814f16afc05
RubinoD1 <Rubinod1@gmail.com>
2022-08-11 18:21:44 -0400

    added starter columns to product model

--- models/Product.js
+++ models/Product.js
@@ -8,19 +8,35 @@ class Product extends Model {}
 
 // set up fields and rules for Product model
 Product.init(
-  {
-      // define an id column
-      id: {
+    {
+        id: {
           type: DataTypes.INTEGER,
           allowNull: false,
           primaryKey: true,
           autoIncrement: true
-      },
-      // define product_name column
-      product_name: {
+        },
+        title: {
           type: DataTypes.STRING,
           allowNull: false
+        },
+        product_info: {
+          type: DataTypes.STRING,
+          allowNull: false
+        },
+        category_id: {
+          type: DataTypes.INTEGER,
+          references: {
+            model: 'category',
+            key: 'id'
+          }
+        }
+      },
+      {
+        sequelize,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'post'
       }
-});
+    );
 
 module.exports = Product;
