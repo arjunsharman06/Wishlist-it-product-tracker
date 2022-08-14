535fdf191a8ca006de0b355c728f9302e43ce27b
RubinoD1 <Rubinod1@gmail.com>
2022-08-14 19:43:46 -0400

    tested working -- updated .env.EXAMPLE

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -20,29 +20,7 @@ router.get('/:id', (req, res) => {
     where: {
       id: req.params.id,
     },
-    /*
-    include: [
-      {
-       // model: Post,
-        attributes: ['id', 'title', 'post_url', 'created_at'],
-      },
-      {
-        model: Comment,
-        attributes: ['id', 'comment_text', 'created_at'],
-        include: {
-          //model: Post,
-          attributes: ['title'],
-        },
-      },
-      {
-        //model: Post,
-        attributes: ['title'],
-        through: Vote,
-        as: 'voted_posts',
-      },
-    ],
-    */
-  })
+})
     .then((dbUserData) => {
       if (!dbUserData) {
         res.status(404).json({ message: 'No user found with this id' });
