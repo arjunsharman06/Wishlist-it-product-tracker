52786c06afbd49894ef071e3deea174bbb267066
RubinoD1 <Rubinod1@gmail.com>
2022-08-14 14:04:02 -0400

    added user seed data

--- config/connection.js
+++ config/connection.js
@@ -1,17 +1,5 @@
 const Sequelize = require('sequelize');
 
-<<<<<<< HEAD
-require('dotenv').config();
-
-// create connection to our db
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-  host: 'localhost',
-  dialect: 'mysql',
-  port: 3306
-});
-
-module.exports = sequelize;
-=======
 // configuration for enviromental variables
 require('dotenv').config();
 
@@ -30,4 +18,3 @@ const sequelize = process.env.JAWSDB_URL
     );
 
 module.exports = sequelize;
->>>>>>> 4cdfcea3e54777f268b1db00a5c08db5924a4760
