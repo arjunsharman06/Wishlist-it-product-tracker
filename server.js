79f0146899853269fb2e3161acb5733b043eda3f
RubinoD1 <Rubinod1@gmail.com>
2022-08-16 20:26:42 -0400

    heroku

--- server.js
+++ server.js
@@ -6,7 +6,9 @@ const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
 
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
